ee2ee7d35a2177b8b153e150588a84418ba478fc
JosueRhea <josueroyale8171@gmail.com>
2021-08-19 16:37:31 -0600

    use notification routes and express json

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const morgan = require("morgan")
 const cors = require("cors")
 
 const songsRoutes = require("./routes/tracks.routes")
+const notificationsRoutes = require("./routes/notifications.routes")
 
 const app = express();
 
 app.use(cors())
+app.use(express.json())
 app.use(morgan("dev"))
 
 //routes
 app.use(songsRoutes)
+app.use(notificationsRoutes)
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server started")
