6fd14d799dc009049bcb1ffd3dcd9b9f4df7778e
JosueRhea <josueroyale8171@gmail.com>
2021-09-18 13:11:19 -0600

    fix origin cors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const songsRoutes = require("./routes/tracks.routes")
-const notificationsRoutes = require("./routes/notifications.routes")
+const songsRoutes = require("./routes/tracks.routes");
+const notificationsRoutes = require("./routes/notifications.routes");
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(morgan("dev"))
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+app.use(express.json());
+app.use(morgan("dev"));
 
 //routes
-app.use(songsRoutes)
-app.use(notificationsRoutes)
+app.use(songsRoutes);
+app.use(notificationsRoutes);
 
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log("Server started")
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server started");
+});
